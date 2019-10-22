cf062cbaf21aa45c7e8c27b6ea7e00b6eafdcb30
peng <414326615@qq.com>
2019-10-22 11:04:31 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const http = require("http");
-const urlPath = require("./urlPath");
-const { CORS } = require("./util");
+const http = require("http"),
+  urlPath = require("./urlPath"),
+  { CORS } = require("./util"),
+  https = require("https"),
+  { keyPath, certPath } = require("./config").https,
+  fs = require("fs"),
+  { server: serConfig } = require("./config");
 
 const server = http
   .createServer((req, res) => {
@@ -15,12 +19,49 @@ const server = http
       return;
     } else if (/notsave/.test(url)) {
       url = "/notsave";
+    } else if (/\/page\//.test(url)) {
+      url = "/page";
     } else if (!(url in urlPath)) {
       url = "notFound";
     }
     urlPath[url](req, res);
   })
-  .listen(8000, "127.0.0.1");
+  .listen(serConfig.httpPort);
 console.log(`
-  server start on http://127.0.0.1:8000
+  server start on http://${serConfig.host}${
+  serConfig.httpPort == 80 ? "" : ":" + serConfig.httpPort
+}
+`);
+
+const options = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath)
+};
+
+const sslServer = https
+  .createServer(options, (req, res) => {
+    // CORS
+    CORS(res);
+    // req.m
+    let url = req.url;
+    if (/minifont/.test(url)) {
+      url = "/minifont";
+      const name = req.url.split("minifont/")[1];
+      urlPath[url](req, res, name);
+      return;
+    } else if (/notsave/.test(url)) {
+      url = "/notsave";
+    } else if (/\/page\//.test(url)) {
+      url = "/page";
+    } else if (!(url in urlPath)) {
+      url = "notFound";
+    }
+    urlPath[url](req, res);
+  })
+  .listen(serConfig.httpsPort);
+
+console.log(`
+server start on https://${serConfig.host}${
+  serConfig.httpsPort == 443 ? "" : ":" + serConfig.httpsPort
+}
 `);
