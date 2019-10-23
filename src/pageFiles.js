e6c66b35493d954bafe954faf6f1e8eead76f41d
peng <414326615@qq.com>
2019-10-23 20:04:46 +0800

    init

--- src/pageFiles.js
+++ src/pageFiles.js
@@ -2,6 +2,27 @@ const fs = require("fs"),
   { methodCheck, errMethod } = require("./util"),
   path = require("path");
 
+const mime = {
+  css: "text/css",
+  gif: "image/gif",
+  html: "text/html",
+  ico: "image/x-icon",
+  jpeg: "image/jpeg",
+  jpg: "image/jpeg",
+  js: "text/javascript",
+  json: "application/json",
+  pdf: "application/pdf",
+  png: "image/png",
+  svg: "image/svg+xml",
+  swf: "application/x-shockwave-flash",
+  tiff: "image/tiff",
+  txt: "text/plain",
+  wav: "audio/x-wav",
+  wma: "audio/x-ms-wma",
+  wmv: "video/x-ms-wmv",
+  xml: "text/xml"
+};
+
 module.exports = function(req, res) {
   if (!methodCheck(req, res, "GET")) return;
 
@@ -13,12 +34,11 @@ module.exports = function(req, res) {
         res.writeHead(500);
         errMethod.serverErr(err, res);
       }
-      if (/index.html/.test(req.url)) {
-        res.writeHead(200, {
-          "Content-Type": "text/html; charset=utf-8"
-        });
-      }
-
+      const extName = path.extname(req.url).split(".")[1];
+      // console.log(extName);
+      res.writeHead(200, {
+        "Content-Type": `${mime[extName] || "text/plain"}`
+      });
       res.end(fileBuf);
     }
   );
