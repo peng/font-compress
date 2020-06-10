3d0cc1c601c6db5768337ae6b5146ffa7cd1ac55
peng <414326615@qq.com>
2020-06-10 23:28:57 +0800

    test: add test

--- test/signin-login/index.test.js
+++
@@ -1,9 +0,0 @@
-const { authUser, chgPassword } = require("../../src/signIn-logIn/index");
-// authUser({
-//   name: "peng",
-//   password: "123456"
-// });
-chgPassword({
-  name: "peng",
-  newPassword: "123456love"
-});
